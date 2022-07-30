5721da7d080aed3aec726051af58afb260cf06a0
David Mendoza <hectormza21@gmail.com>
2022-07-29 20:08:42 -0400

    mobile css work

--- src/routes/login/SignUp.jsx
+++ src/routes/login/SignUp.jsx
@@ -34,7 +34,6 @@ function SignUp({ setIsUserSigninUp, SignUpSubmitHandler }) {
         <button type="submit" className={styles.signUpButton}>
           Sign Up
         </button>
-        <hr />
         <div className={styles.signInSwitch}>
           <p>Already have an account?</p>
           <button type="button" onClick={() => setIsUserSigninUp(false)}>
