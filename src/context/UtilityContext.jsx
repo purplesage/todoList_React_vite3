efd753cf02acd104bc2ddabe970be15c3184b20b
David Mendoza <hectormza21@gmail.com>
2022-08-13 14:10:40 -0400

    before breaking changes.

---
+++ src/context/UtilityContext.jsx
@@ -0,0 +1,39 @@
+import React, { createContext } from "react";
+
+const utilityContext = createContext({});
+
+function UtilityContext({ children }) {
+  //* content availability state.
+  const [error, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [isMobileNav, setIsMobileNav] = useState(false);
+  const [notificationDisplay, setNotificationDisplay] = useState(false);
+
+  const handleNotificationAnimation = () => {
+    setNotificationDisplay(true);
+
+    setTimeout(() => {
+      setNotificationDisplay(false);
+    }, 4500);
+  };
+
+  return (
+    <utilityContext.Provider
+      value={{
+        error,
+        setIsError,
+        isLoading,
+        setIsLoading,
+        isMobileNav,
+        setIsMobileNav,
+        notificationDisplay,
+        handleNotificationAnimation,
+      }}
+    >
+      {children}
+    </utilityContext.Provider>
+  );
+}
+
+export default UtilityContext;
