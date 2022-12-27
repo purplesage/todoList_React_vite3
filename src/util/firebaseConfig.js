aad8509486f3517e0a928b24f0b722ded7248c26
purpleSage <hectormza21@gmail.com>
2022-12-26 20:49:10 -0400

    host bug fix (switching to vercel)

--- src/util/firebaseConfig.js
+++ src/util/firebaseConfig.js
@@ -1,18 +1,15 @@
-import fireBaseAPIkey from "../apikey";
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import {getStorage} from "firebase/storage";
-
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: fireBaseAPIkey,
-  authDomain: "todo-list-c49be.firebaseapp.com",
-  projectId: "todo-list-c49be",
-  storageBucket: "todo-list-c49be.appspot.com",
-  messagingSenderId: "780376709778",
-  appId: "1:780376709778:web:3dea9bc644779a4d01e44b",
+	apiKey: import.meta.env.VITE_FIREBASE_KEY,
+	authDomain: 'todo-list-c49be.firebaseapp.com',
+	projectId: 'todo-list-c49be',
+	storageBucket: 'todo-list-c49be.appspot.com',
+	messagingSenderId: '780376709778',
+	appId: '1:780376709778:web:3dea9bc644779a4d01e44b',
 };
 
 export const app = initializeApp(firebaseConfig);
@@ -20,4 +17,3 @@ export const dataBase = getFirestore(app);
 export const auth = getAuth(app);
 export const storage = getStorage(app);
 export const googleProvider = new GoogleAuthProvider();
-
