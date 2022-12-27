e76659d13a7d022a16d17368bbee2ceb2c89bbf3
purpleSage <hectormza21@gmail.com>
2022-12-26 20:05:35 -0400

    note preformat fix, autofocus mobile bugfix

--- src/components/FolderContent.jsx
+++ src/components/FolderContent.jsx
@@ -1,127 +1,126 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
-import styles from "../styles/modules/folderContent.module.css";
-import NoteList from "./NoteList";
-import FileList from "./FileList";
-import AddNoteForm from "./AddNoteForm";
-import AddFileForm from "./AddFileForm";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import styles from '../styles/modules/folderContent.module.css';
+import NoteList from './NoteList';
+import FileList from './FileList';
+import AddNoteForm from './AddNoteForm';
+import AddFileForm from './AddFileForm';
 
-import Masonry from "@mui/lab/Masonry";
-import { MdOutlineStickyNote2 } from "react-icons/md";
-import { AiFillFolder } from "react-icons/ai";
-import { CgClose } from "react-icons/cg";
-import { AiFillFileAdd } from "react-icons/ai";
-import { MdStorage } from "react-icons/md";
+import Masonry from '@mui/lab/Masonry';
+import { MdOutlineStickyNote2 } from 'react-icons/md';
+import { AiFillFolder } from 'react-icons/ai';
+import { CgClose } from 'react-icons/cg';
+import { AiFillFileAdd } from 'react-icons/ai';
+import { MdStorage } from 'react-icons/md';
 
 function FolderContent({ notes, folderTitle, setShowFile, folderID, files }) {
-  const [showNoteInputs, setShowNoteInputs] = useState(false);
-  const [showFileInputs, setShowFileInputs] = useState(false);
+	const [showNoteInputs, setShowNoteInputs] = useState(false);
+	const [showFileInputs, setShowFileInputs] = useState(false);
 
-  const [displayStorageFiles, setDisplayStorageFiles] = useState(false);
+	const [displayStorageFiles, setDisplayStorageFiles] = useState(false);
 
-  const [searchValue, setSearchValue] = useState("");
+	const [searchValue, setSearchValue] = useState('');
 
-  return ReactDOM.createPortal(
-    <main className={styles.folderGrid}>
-      <h1>
-        <p className={styles.folderTitle}>
-          <AiFillFolder />
-          {folderTitle}
-        </p>
-        <button
-          type="button"
-          onClick={() => {
-            setShowFile(false);
-          }}
-        >
-          <CgClose />
-        </button>
-      </h1>
+	return ReactDOM.createPortal(
+		<main className={styles.folderGrid}>
+			<h1>
+				<p className={styles.folderTitle}>
+					<AiFillFolder />
+					{folderTitle}
+				</p>
+				<button
+					type="button"
+					onClick={() => {
+						setShowFile(false);
+					}}
+				>
+					<CgClose />
+				</button>
+			</h1>
 
-      <input
-        autoFocus
-        maxLength="25"
-        autoComplete="off"
-        className={styles.searchBar}
-        value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
-        type="text"
-        name="search"
-        id="search"
-        placeholder="Search note:"
-      />
+			<input
+				maxLength="25"
+				autoComplete="off"
+				className={styles.searchBar}
+				value={searchValue}
+				onChange={(e) => setSearchValue(e.target.value)}
+				type="text"
+				name="search"
+				id="search"
+				placeholder="Search note:"
+			/>
 
-      <section className={styles.folderBodyNoteGrid}>
-        <Masonry columns={{ xs: 1, sm: 4 }} spacing={2}>
-          <NoteList
-            notes={notes}
-            folderID={folderID}
-            searchValue={searchValue}
-          />
-        </Masonry>
-      </section>
+			<section className={styles.folderBodyNoteGrid}>
+				<Masonry columns={{ xs: 1, sm: 4 }} spacing={2}>
+					<NoteList
+						notes={notes}
+						folderID={folderID}
+						searchValue={searchValue}
+					/>
+				</Masonry>
+			</section>
 
-      {/* buttons that toggle form display */}
+			{/* buttons that toggle form display */}
 
-      <button
-        className={styles.openFileStorage}
-        type="button"
-        onClick={() => setDisplayStorageFiles(true)}
-      >
-        <MdStorage />
-      </button>
+			<button
+				className={styles.openFileStorage}
+				type="button"
+				onClick={() => setDisplayStorageFiles(true)}
+			>
+				<MdStorage />
+			</button>
 
-      <button
-        className={styles.openAddNoteForm}
-        type="button"
-        onClick={() => setShowNoteInputs(true)}
-      >
-        <MdOutlineStickyNote2 />
-      </button>
+			<button
+				className={styles.openAddNoteForm}
+				type="button"
+				onClick={() => setShowNoteInputs(true)}
+			>
+				<MdOutlineStickyNote2 />
+			</button>
 
-      {showNoteInputs && (
-        <AddNoteForm
-          folderID={folderID}
-          setShowNoteInputs={setShowNoteInputs}
-        />
-      )}
+			{showNoteInputs && (
+				<AddNoteForm
+					folderID={folderID}
+					setShowNoteInputs={setShowNoteInputs}
+				/>
+			)}
 
-      {displayStorageFiles && (
-        <section className={styles.folderBodyFileGrid}>
-          <h1>
-            Folder Storage:{" "}
-            <button type="button" onClick={() => setDisplayStorageFiles(false)}>
-              <CgClose />
-            </button>
-          </h1>
+			{displayStorageFiles && (
+				<section className={styles.folderBodyFileGrid}>
+					<h1>
+						Folder Storage:{' '}
+						<button type="button" onClick={() => setDisplayStorageFiles(false)}>
+							<CgClose />
+						</button>
+					</h1>
 
-          <div className={styles.folderBodyFileFlex}>
-            <FileList
-              files={files}
-              folderTitle={folderTitle}
-              folderID={folderID}
-            />
-            {!showFileInputs ? (
-              <button
-                className={styles.addFileButton}
-                type="button"
-                onClick={() => setShowFileInputs(true)}
-              >
-                <AiFillFileAdd />
-              </button>
-            ) : (
-              <AddFileForm
-                setShowFileInputs={setShowFileInputs}
-                folderTitle={folderTitle}
-                folderID={folderID}
-              />
-            )}
-          </div>
-        </section>
-      )}
-    </main>,
-    document.getElementById("portal")
-  );
+					<div className={styles.folderBodyFileFlex}>
+						<FileList
+							files={files}
+							folderTitle={folderTitle}
+							folderID={folderID}
+						/>
+						{!showFileInputs ? (
+							<button
+								className={styles.addFileButton}
+								type="button"
+								onClick={() => setShowFileInputs(true)}
+							>
+								<AiFillFileAdd />
+							</button>
+						) : (
+							<AddFileForm
+								setShowFileInputs={setShowFileInputs}
+								folderTitle={folderTitle}
+								folderID={folderID}
+							/>
+						)}
+					</div>
+				</section>
+			)}
+		</main>,
+		document.getElementById('portal')
+	);
 }
 
 export default FolderContent;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,42 +1,44 @@
-import React, { useState, useContext } from "react";
-import styles from "../styles/modules/note.module.css";
-import { folderListContext } from "../context/FolderListContext";
+import React, { useState, useContext } from 'react';
+import styles from '../styles/modules/note.module.css';
+import { folderListContext } from '../context/FolderListContext';
 
-import { CgClose } from "react-icons/cg";
+import { CgClose } from 'react-icons/cg';
 
 function Note({ noteId, noteTitle, folderID, noteBody }) {
-  const [modalStyle, setModalStyle] = useState(false);
+	const [modalStyle, setModalStyle] = useState(false);
 
-  const { handleDeleteNote } = useContext(folderListContext);
+	const { handleDeleteNote } = useContext(folderListContext);
 
-  const customStyle = () => {
-    return {
-      position: "absolute",
-      top: "35vh",
-      right: "40vw",
-      border: "rgb(163, 45, 110) 4px dashed",
-      transform: "scale(1.3)",
-    };
-  };
+	const customStyle = () => {
+		return {
+			position: 'absolute',
+			top: '35vh',
+			right: '40vw',
+			border: 'rgb(163, 45, 110) 4px dashed',
+			transform: 'scale(1.3)',
+		};
+	};
 
-  return (
-    <div
-      style={modalStyle ? customStyle() : null}
-      onDoubleClick={() => setModalStyle(!modalStyle)}
-      className={styles.note}
-    >
-      <div className={styles.noteTitleDiv}>
-        <h2>{noteTitle}</h2>
-        <button
-          type="button"
-          onClick={() => handleDeleteNote(noteId, folderID)}
-        >
-          <CgClose />
-        </button>
-      </div>
-      <p>{noteBody}</p>
-    </div>
-  );
+	return (
+		<div
+			style={modalStyle ? customStyle() : null}
+			onDoubleClick={() => setModalStyle(!modalStyle)}
+			className={styles.note}
+		>
+			<div className={styles.noteTitleDiv}>
+				<h2>{noteTitle}</h2>
+				<button
+					type="button"
+					onClick={() => handleDeleteNote(noteId, folderID)}
+				>
+					<CgClose />
+				</button>
+			</div>
+			<pre>
+				<p>{noteBody}</p>
+			</pre>
+		</div>
+	);
 }
 
 export default Note;
